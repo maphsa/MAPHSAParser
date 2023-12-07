41381bb2a92752bc14f5fbc596d0befcc34a768e
atapscott <alan.tapscott@gmail.com>
2023-12-07 11:46:11 +0100

    Support supplementary geometry args.

--- command.py
+++ command.py
@@ -16,6 +16,7 @@ class Subcommands(Enum):
 cli_parser = argparse.ArgumentParser()
 cli_parser.add_argument('subcommand', nargs='+', type=str, help='main subcommand')
 cli_parser.add_argument('-i', '--input', nargs='*', type=pathlib.Path, help='input files')
+cli_parser.add_argument('-sg', '--supp', action='store_true', help='add supplementary geo')
 cli_parser.add_argument('-f', '--fake', action='store_true', help='fake insertion')
 
 args: argparse.Namespace = cli_parser.parse_args()
