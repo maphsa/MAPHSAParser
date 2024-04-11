e5e5d791f084b662ae1df17fcc294dc66ca1f5b7
atapscott <alan.tapscott@gmail.com>
2024-04-11 17:42:36 +0200

    Add print concepts capability

--- arches_interface/__init__.py
+++ arches_interface/__init__.py
@@ -2,5 +2,6 @@ from arches_interface import arches_interface
 from arches_interface import thesaurus_parser
 
 LOAD_CONCEPTS_SUBCOMMAND = 'load_concepts'
+PRINT_CONCEPTS_SUBCOMMAND = 'print_concepts'
 
-arches_interface_subcommands = [LOAD_CONCEPTS_SUBCOMMAND]
+arches_interface_subcommands = [LOAD_CONCEPTS_SUBCOMMAND, PRINT_CONCEPTS_SUBCOMMAND]
